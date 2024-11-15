47d7c3384ffc6b67822d77b73f2f914611cb2f89
Mitch Dzugan <mitchdzugan@gmail.com>
2024-11-15 16:08:53 -0600

    use better flake template

--- flake.nix
+++ flake.nix
@@ -1,14 +1,35 @@
 {
   description = "latest version of gtk-server with gtk4 support";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system: with import nixpkgs { system = system; }; {
-        packages.default = stdenv.mkDerivation rec {
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      # Generate a user-friendly version number.
+      version = "2.4.7";
+
+      # System types to support.
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system: with import nixpkgs { system = system; }; {
+        gtk-server = nixpkgsFor.${system}.stdenv.mkDerivation {
           pname = "gtk-server";
-          version = "2.4.7";
+          inherit version;
 
           src = fetchurl {
             url = "https://www.gtk-server.org/stable/gtk-server-${version}.tar.gz";
@@ -33,4 +54,18 @@
           };
         };
       });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.gtk-server);
+
+      devShell = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        pkgs.mkShell { buildInputs = with pkgs; [ ]; }
+      );
+    };
 }
